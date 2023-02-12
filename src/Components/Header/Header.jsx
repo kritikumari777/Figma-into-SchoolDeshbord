c543f2274b4895d4cec7cf96d099b05ce33e21f6
Kriti <123kritikumari@gmail.com>
2023-02-12 09:08:10 +0530

    basic setup

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import styles from "./Header.module.scss"
+const Header = () => {
+    return (
+        <div className={styles["header-wrapper"]}>
+            Kriti
+        </div>
+    )
+}
+
+export default Header
---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../Components/Header/Header'
+
+import styles from './LendingPage.module.scss'
+
+const LandingPage = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+export default LandingPage
